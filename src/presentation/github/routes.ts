56afbead6911b9259008fb20ea8482e2f7449064
Patricio <native.devil@gmail.com>
2025-08-03 19:30:35 -0400

    REFACT: Unnecesary brackets

--- src/presentation/github/routes.ts
+++ src/presentation/github/routes.ts
@@ -2,4 +2,4 @@ import { Elysia } from "elysia";
 import { GithubService } from "../services";
 
 export const githubRoutes = new Elysia()
-  .post( '/', ctx => ( GithubService.webhookHandler(ctx) ) );
+  .post('/', ctx => GithubService.webhookHandler(ctx));
