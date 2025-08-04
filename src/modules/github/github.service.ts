78547c1cf0a05540c78543753496a61f84a9391f
Patricio <native.devil@gmail.com>
2025-08-04 12:25:26 -0400

    ADD: Event handler

--- src/modules/github/github.service.ts
+++ src/modules/github/github.service.ts
@@ -1,15 +1,38 @@
 import { Context, status } from "elysia"
+import { GithubStarPayload } from "../../interfaces";
+import { github } from ".";
 
 export abstract class GithubService {
   constructor(){}
 
-  static webhookHandler({body, headers}:Context){
+  static webhookHandler({body, headers}: Context & { body: any }){
     const event = headers['x-github-event'] ?? 'Unknown';
-    const signature = headers['x-hub-signature-256'] ?? 'Unknown';
-    // console.log(ctx.body);
-    // console.log("Endpoint Llamado!");
-    console.log({ event, signature });
+    // const signature = headers['x-hub-signature-256'] ?? 'Unknown';
+    const { action } = body;
+    // const githubEvent:string = body.action ?? '';
+    console.log(event === 'star')
     
+    switch(event){
+      case 'star':
+        const starBody = body as GithubStarPayload;
+        console.log(this.onStar(starBody));
+        break;
+      default:
+        console.log(`Unknown handler for event: ${ event }`);
+        break;
+    }
+
     return status(202, { message: "Accepted" });
+  }
+
+  static onStar(payload: GithubStarPayload){
+    let message:string = '';
+    const { action, sender, repository, starred_at } = payload;
+
+    if(starred_at){
+      message = `User ${sender.login} ${action} star on ${repository.name} repository`;
+    }
+
+    return message;
   }
 }
