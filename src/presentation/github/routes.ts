94a1eba48d15a33d12ee194a605017182bf4d9c3
Patricio <native.devil@gmail.com>
2025-08-03 21:03:41 -0400

    REFACT: Routes Prefix

--- src/presentation/github/routes.ts
+++ src/presentation/github/routes.ts
@@ -1,5 +1,5 @@
 import { Elysia } from "elysia";
 import { GithubService } from "../services";
 
-export const githubRoutes = new Elysia()
+export const githubRoutes = new Elysia({ prefix: '/github' })
   .post('/', ctx => GithubService.webhookHandler(ctx));
--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,9 +1,5 @@
 import { Elysia } from "elysia";
 import { githubRoutes } from "./github/routes";
 
-export const appRoutes = new Elysia()
-  .group("/api", app =>
-    app.group("/github", app =>
-      app.use(githubRoutes)
-    )
-  );
+export const appRoutes = new Elysia({ prefix: '/api'})
+  .use(githubRoutes);
